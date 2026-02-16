ee923e2517e8c655ec0ee30a5d883b8f3e846a2c
Subham Samanta <subham@synergicsoftek.in>
2026-02-16 16:20:17 +0530

    Update in common model 16.02.2026

--- model/CommonModel.js
+++ model/CommonModel.js
@@ -6,7 +6,7 @@ const getBlockList = (ardb_id, block_id=0) => {
             let select = '*',
                 table_name = 'md_block',
                 whr = `ardb_id = ${ardb_id} ${block_id > 0 ? `AND block_id = ${block_id}` : ''}`;
-            const res_dt = await F_Select(0, select, table_name, whr, null, 1)
+            const res_dt = await F_Select(0, select, table_name, whr, `ORDER BY block_name ASC`, 1)
             resolve(res_dt);
         }catch(err){
             resolve({suc: 0, msg: err})
@@ -20,7 +20,7 @@ const getServiceAreaList = (ardb_id, block_id, service_area_id = 0) => {
             let select = 'a.*, b.block_name',
                 table_name = 'md_service_area a, md_block b',
                 whr = `a.ardb_id=b.ardb_id AND a.block_id=b.block_id AND a.ardb_id = ${ardb_id} AND a.block_id = ${block_id} ${service_area_id > 0 ? `AND a.service_area_id = ${service_area_id}` : ''}`;
-            const res_dt = await F_Select(0, select, table_name, whr, null, 1)
+            const res_dt = await F_Select(0, select, table_name, whr, `ORDER BY a.service_area_name ASC`, 1)
             resolve(res_dt);
         } catch (err) {
             resolve({ suc: 0, msg: err })
@@ -35,7 +35,7 @@ const getVillageList = (ardb_id, block_id, service_area_id, vill_id = 0) => {
             let select = 'a.*, b.service_area_name, c.block_name',
                 table_name = 'md_village a, md_service_area b, md_block c',
                 whr = `a.ardb_id=b.ardb_id AND a.block_id=b.block_id AND a.service_area_id=b.service_area_id AND a.ardb_id=c.ardb_id AND a.block_id=c.block_id AND a.ardb_id = ${ardb_id} AND a.block_id = '${block_id}' AND a.service_area_id IN(${service_area_id}) ${vill_id > 0 ? `AND a.vill_id = '${vill_id}'` : ''}`;
-            const res_dt = await F_Select(0, select, table_name, whr, null, 1)
+            const res_dt = await F_Select(0, select, table_name, whr, `ORDER BY a.vill_name ASC`, 1)
             resolve(res_dt);
         } catch (err) {
             resolve({ suc: 0, msg: err })
