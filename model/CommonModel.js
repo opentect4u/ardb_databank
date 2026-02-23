39a667c867e36ed0d13b5496ff8b038231a4a338
Subham Samanta <subham@synergicsoftek.in>
2026-02-23 15:46:28 +0530

    Update common model and supervisor entry page 23.02.2026

--- model/CommonModel.js
+++ model/CommonModel.js
@@ -4,7 +4,7 @@ const getBlockList = (ardb_id, block_id=0) => {
     return new Promise(async (resolve, reject) => {
         try{
             let select = '*',
-                table_name = 'md_block',
+                table_name = 'v_block',
                 whr = `ardb_id = ${ardb_id} ${block_id > 0 ? `AND block_id = ${block_id}` : ''}`;
             const res_dt = await F_Select(0, select, table_name, whr, `ORDER BY block_name ASC`, 1)
             resolve(res_dt);
@@ -18,7 +18,7 @@ const getServiceAreaList = (ardb_id, block_id, service_area_id = 0) => {
     return new Promise(async (resolve, reject) => {
         try {
             let select = 'a.*, b.block_name',
-                table_name = 'md_service_area a, md_block b',
+                table_name = 'v_service_area a, v_block b',
                 whr = `a.ardb_id=b.ardb_id AND a.block_id=b.block_id AND a.ardb_id = ${ardb_id} AND a.block_id = ${block_id} ${service_area_id > 0 ? `AND a.service_area_id = ${service_area_id}` : ''}`;
             const res_dt = await F_Select(0, select, table_name, whr, `ORDER BY a.service_area_name ASC`, 1)
             resolve(res_dt);
@@ -31,12 +31,16 @@ const getServiceAreaList = (ardb_id, block_id, service_area_id = 0) => {
 const getVillageList = (ardb_id, block_id, service_area_id, vill_id = 0) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let service_area = Array.isArray(service_area_id) ? service_area_id.join(',') : service_area_id
-            let select = 'a.*, b.service_area_name, c.block_name',
-                table_name = 'md_village a, md_service_area b, md_block c',
-                whr = `a.ardb_id=b.ardb_id AND a.block_id=b.block_id AND a.service_area_id=b.service_area_id AND a.ardb_id=c.ardb_id AND a.block_id=c.block_id AND a.ardb_id = ${ardb_id} AND a.block_id = '${block_id}' AND a.service_area_id IN(${service_area_id}) ${vill_id > 0 ? `AND a.vill_id = '${vill_id}'` : ''}`;
-            const res_dt = await F_Select(0, select, table_name, whr, `ORDER BY a.vill_name ASC`, 1)
-            resolve(res_dt);
+            let service_area = Array.isArray(service_area_id) ? service_area_id.map(dt => dt.toString()).join(',') : service_area_id
+            if (service_area != 'all'){
+                let select = 'a.*, b.service_area_name, c.block_name',
+                    table_name = 'v_vill a, v_service_area b, v_block c',
+                    whr = `a.ardb_id=b.ardb_id AND a.block_id=b.block_id AND a.service_area_id=b.service_area_id AND a.ardb_id=c.ardb_id AND a.block_id=c.block_id AND a.ardb_id = ${ardb_id} AND a.block_id = '${block_id}' AND a.service_area_id IN(${service_area}) ${vill_id > 0 ? `AND a.vill_id = '${vill_id}'` : ''}`;
+                const res_dt = await F_Select(0, select, table_name, whr, `ORDER BY a.vill_name ASC`, 1)
+                resolve(res_dt);
+            }else{
+                resolve({suc: 1, msg: []});
+            }
         } catch (err) {
             resolve({ suc: 0, msg: err })
         }
