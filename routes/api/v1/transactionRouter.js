f1d9341052095eaf1914b61e99195bbef237e030
Subham Samanta <subham@synergicsoftek.in>
2026-02-27 15:34:33 +0530

    Update in transaction 27.02.2026

--- routes/api/v1/transactionRouter.js
+++ routes/api/v1/transactionRouter.js
@@ -115,7 +115,7 @@ transRouter.post('/transaction', async (req, res) => {
                             user_ac_info.product_type_code,
                             user_ac_info.product_id,
                             user_ac_info.cust_name,
-                            dateFormat(value.intt_calc_dt, 'mm-dd') == '03-31' ? 'N' : 'Y',
+                            value.intt_calc_dt && value.intt_calc_dt != '' ? (dateFormat(value.intt_calc_dt, 'mm-dd') != '03-31' ? 'N' : 'Y') : 'N',
                             dateFormat(value.intt_calc_dt, 'yyyy-mm-dd'),
                             dateFormat(value.recov_dt, "yyyy-mm-dd"),
                             value.tot_recov,
